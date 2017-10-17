a122a2a43021bb31ab7cde92b5eb0d8d4f2e6f29
alexandrejuk <ale_santos.soares@hotmail.com>
2017-10-17 10:25:49 -0200

    fix: some bugs in compoentn home

--- src/app/pages/home/content/content.component.ts
+++ src/app/pages/home/content/content.component.ts
@@ -9,10 +9,10 @@ export class ContentComponent implements OnInit {
 
 
   tutors = [
-    { name: 'Node', descriptions: '', img: 'assets/img/angular.jpg', autor: 'Alexandre dos Santos Soares', hours: '9 horas', rate: 4.5 },
-    { name: 'Css', descriptions: '', img: 'assets/img/css.png', autor: 'Vitor Silva Lima', hours: '8 horas', rate: 4.4 },
-    { name: 'Js', descriptions: '', img: 'assets/img/js.png', autor: 'Caio Santos', hours: '7.5 horas', rate: 4.0 },
-    { name: 'Html', descriptions: '', img: 'assets/img/html5.png', autor: 'Alan Brado', hours: '5 horas', rate: 4.1 },
+    { name: 'Node', descriptions: '', img: 'assets/img/angular.jpg', autor: 'Alexandre dos Santos', hours: '9 horas', rate: 4.5 },
+    { name: 'Css', descriptions: '', img: 'assets/img/angular.jpg', autor: 'Vitor Silva Lima', hours: '8 horas', rate: 4.4 },
+    { name: 'Js', descriptions: '', img: 'assets/img/angular.jpg', autor: 'Caio Santos', hours: '7.5 horas', rate: 4.0 },
+    { name: 'Html', descriptions: '', img: 'assets/img/angular.jpg', autor: 'Alan Brado', hours: '5 horas', rate: 4.1 },
   ];
 
   constructor() { }
