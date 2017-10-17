c3d87d220c44c65dfca9a1378259b79db921dd58
alexandrejuk <ale_santos.soares@hotmail.com>
2017-10-17 19:13:14 -0200

    chore: new components and add new elements in navbar

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { routing } from './home.routing';
 import { ContentComponent } from './content/content.component';
+import { LoginModule } from '../login/login.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    LoginModule,
     routing
   ],
   declarations: [HomeComponent, ContentComponent]
