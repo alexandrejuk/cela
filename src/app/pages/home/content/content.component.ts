938db66fec1b7b7b25149f7cb7676e0645df6a8e
alexandrejuk <ale_santos.soares@hotmail.com>
2017-10-17 10:29:56 -0200

    chore: add and remove some imgs

--- src/app/pages/home/content/content.component.ts
+++ src/app/pages/home/content/content.component.ts
@@ -9,10 +9,10 @@ export class ContentComponent implements OnInit {
 
 
   tutors = [
-    { name: 'Node', descriptions: '', img: 'assets/img/angular.jpg', autor: 'Alexandre dos Santos', hours: '9 horas', rate: 4.5 },
-    { name: 'Css', descriptions: '', img: 'assets/img/angular.jpg', autor: 'Vitor Silva Lima', hours: '8 horas', rate: 4.4 },
-    { name: 'Js', descriptions: '', img: 'assets/img/angular.jpg', autor: 'Caio Santos', hours: '7.5 horas', rate: 4.0 },
-    { name: 'Html', descriptions: '', img: 'assets/img/angular.jpg', autor: 'Alan Brado', hours: '5 horas', rate: 4.1 },
+    { name: 'Node', descriptions: '', img: 'assets/img/node.jpg', autor: 'Alexandre dos Santos', hours: '9 horas', rate: 4.5 },
+    { name: 'React', descriptions: '', img: 'assets/img/css.jpg', autor: 'Vitor Silva Lima', hours: '8 horas', rate: 4.4 },
+    { name: 'Js and Firebase', descriptions: '', img: 'assets/img/js.jpg', autor: 'Caio Santos', hours: '7.5 horas', rate: 4.0 },
+    { name: 'Angular', descriptions: '', img: 'assets/img/angular.jpg', autor: 'Alan Brado', hours: '5 horas', rate: 4.1 },
   ];
 
   constructor() { }
